800c4b84e6c1e67bf1048ca0224d59d480d8b163
Julia Shuyue Wu <jules.wu@Julias-MacBook-Air.local>
2016-11-03 17:10:47 -0400

    first commit for bshw3.py

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -1,3 +1,13 @@
+#name: Julia Wu
+#UMID: 38632603
+#Unique name: shuyue
+#References: 
+
+import re
+import requests
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
 # Use https://www.si.umich.edu/programs/bachelor-science-
 # information/bsi-admissions as a template.
 # STEPS 
@@ -10,3 +20,33 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+f = open('webpage.html', 'w')
+
+base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
+
+html_doc = urllib.request.urlopen(base_url).read()
+
+soup_1 = BeautifulSoup(html_doc, 'html.parser')
+
+filename = '/Users/jules.wu/Desktop/Project.3/HW3-StudentCopy/media/logo.png'
+
+visible_text = (soup_1.prettify())
+#print (visible_text)
+
+im_str = visible_text.replace('/sites/default/themes/umsi/imgs/logo.png',filename)
+
+text = im_str.replace('students', 'AMAZING student')
+
+#images = re.findall('^/sites/default/themes/umsi/img\S+', text)
+#print (images)
+
+f.write(text)
+f.close()
+
+#filename = '/Users/jules.wu/Desktop/Project.3/HW3-StudentCopy/webpage.html'
+#webbrowser.open_new_tab(filename)
+
+###
+#Find the picture and change img source (specific pic) and replace it with your picture
+#replace logo picture (use regular expressions to do this)
