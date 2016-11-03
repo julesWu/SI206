88bdda6096ac56bb52bed80977b810e0f835a556
Julia Shuyue Wu <jules.wu@Julias-MacBook-Air.local>
2016-11-03 08:51:01 -0400

    first commit

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -1,3 +1,16 @@
+#name: Julia Wu
+#UMID: 38632603
+#Unique name: shuyue
+#References: 
+
+import nltk
+import random
+
+# import nltk
+nltk.download('punkt')
+
+from nltk import word_tokenize,sent_tokenize
+
 # Using text2 from the nltk book corpa, create your own version of the
 # MadLib program.  
 
@@ -9,7 +22,42 @@
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
+
 print("START*******")
 
+debug = False
+
+sense = nltk.corpus.gutenberg.raw('austen-sense.txt')
+#print (sense[0:150])
+tokens = nltk.word_tokenize(sense)
+print("TOKENS")
+print(tokens[0:150])
+tagged_tokens = nltk.pos_tag(tokens[:150]) # gives us a tagged list of tuples
+#print("TAGGED TOKENS")
+#print(tagged_tokens)
+#if debug:
+	#print ("First few tagged tokens are:")
+	#for tup in tagged_tokens[:5]:
+		#print (tup)
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective", "VBD": "a past-tense verb"}
+substitution_probabilities = {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1, "VBD":.1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
 
 print("\n\nEND*******")
