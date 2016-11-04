023979182e0c769733faeaf2b244b2c51af0eb94
Julia Shuyue Wu <jules.wu@Julias-MacBook-Air.local>
2016-11-04 18:42:42 -0400

    Finished coding, added comments, and made code look clean

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -3,7 +3,6 @@
 #Unique name: shuyue
 #References: 
 
-import re
 import requests
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
@@ -21,31 +20,40 @@ from bs4 import BeautifulSoup
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
 
+#opens the html file and prepares the file for writing
 f = open('webpage.html', 'w')
 
+#the url that we are using
 base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
 
+#makes a request to the base_url, opens, and reads the webpage
 html_doc = urllib.request.urlopen(base_url).read()
 
+#use Beautiful soup to parse the webpage
 soup_1 = BeautifulSoup(html_doc, 'html.parser')
 
+#Path of the logo picture Collen wanted us to use.
 filename = '/Users/jules.wu/Desktop/Real.3/HW3-StudentCopy/media/logo.png'
+
+#Path of the picture of ourselves that we were supposed to use
 my_pic = '/Users/jules.wu/Desktop/Real.3/HW3-StudentCopy/media/Pic.png'
 
+#makes the code of the base_url page pretty (readable)
 visible_text = (soup_1.prettify())
-#print (visible_text)
 
+#looks through visible text and replaces the missing logos with our version of the logo
 im_str = visible_text.replace('logo2.png',filename)
+
+#looks through im_str to replace the big picture with a picture of myself
 im_str3 = im_str.replace('https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg', my_pic)
 
+#looks through im_str3, and replaces any occurance of 'students' with 'AMAZING students'
 text = im_str3.replace('students', 'AMAZING students')
 
-#images = re.findall('^/sites/default/themes/umsi/img\S+', text)
-#print (images)
-
+#writes the text to the html file
 f.write(text)
+
+#closes the file
 f.close()
 
-###
-#Find the picture and change img source (specific pic) and replace it with your picture
-#replace logo picture (use regular expressions to do this)
+
