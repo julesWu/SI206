7bf2c1d7d44fa3dc5ddb8f6a706cd804d8036a2b
Julia Shuyue Wu <jules.wu@Julias-MacBook-Air.local>
2016-11-05 16:38:33 -0400

    commented code and changed picture path

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -13,17 +13,23 @@ consumer_key = "nRNVXexELjn4ZMSB5APdmnbnU"
 consumer_secret = "fsPYlzDIcxeHy42fUV736nOqIoqo0fNI4BkENokc4h6sClIGS3"
 
 # Boilerplate code here
+# function to use the twitter API
 def get_api():
 	auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 	auth.set_access_token(access_token,access_token_secret)
 	return tweepy.API(auth)
 
 #Now we can Create Tweets, Delete Tweets, and Find Twitter Users
-
+# api gets from Twitter
 api = get_api()
-tweet = "Picking apples #UMSI-206 #Proj3"
+
+#variable containing tweet information
+tweet = "UMICH looking lovely as always #UMSI-206 #Proj3"
+
+#path of the picture I want to upload
 filename = '/Users/jules.wu/Desktop/Pic.jpg'
+
+#uploads the tweet and picture onto Itwitter
 status = api.update_with_media(filename, status = tweet)
 
-#print("""No output necessary although you 
-	#can print out a success/failure message if you want to.""")
+print("Twitter Updated! Have a great day!")
